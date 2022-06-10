3756f2c3a6c455659a6df18d60287557ad3e4062
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-06-10 09:33:53 +0700

    test: Test Get List Posts Query With Title, Test Get List Posts Unauthorized

--- test/post_test.go
+++ test/post_test.go
@@ -305,7 +305,7 @@ func TestGetListPostsSuccessWithIsAdminTrue(t *testing.T) {
 	assert.NotEqual(t, 0, countAuthor)
 }
 
-// TestGetListPostsSuccessWithIsAdminTrue as find list post with exception not showing post owned user is logged in
+// TestGetListPostsSuccessWithIsAdminFalse as find list post with exception not showing post owned user is logged in
 func TestGetListPostsSuccessWithIsAdminFalse(t *testing.T) {
 	// Open connection to db
 	db := util.SetupTestDb()
@@ -379,5 +379,91 @@ func TestGetListPostsSuccessWithIsAdminFalse(t *testing.T) {
 	assert.Equal(t, 0, countAuthor)
 }
 
-// Todo: get all list post query title
-// Todo: get all list post unauthorized
+// TestGetListPostsQueryWithTitle as find list post with filter by title
+func TestGetListPostsQueryWithTitle(t *testing.T) {
+	// Open connection to db
+	db := util.SetupTestDb()
+
+	// Call router with argument db
+	router := router.SetupRouter(db)
+
+	// Generate random post, and get token used to create this post for check if author post is in the list
+	newPost, strToken := createRandomPost(t, true)
+
+	// Create request with query filter by title
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/api/posts?title="+newPost.Title, nil)
+	// Added header content type
+	request.Header.Add("Content-Type", "application/json")
+	// Added header Authorization with by inserting jwt token
+	request.Header.Add("Authorization", strToken)
+
+	// Create recorder
+	recorder := httptest.NewRecorder()
+
+	// Run server http
+	router.ServeHTTP(recorder, request)
+
+	// Get response
+	response := recorder.Result()
+
+	// Read response
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	// Decode json
+	json.Unmarshal(body, &responseBody)
+
+	// Response status code must be 200 (success)
+	assert.Equal(t, 200, response.StatusCode)
+	// Response body status code must be 200 (success)
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	// Response body status must be success
+	assert.Equal(t, "success", responseBody["status"])
+	// Response body message
+	assert.Equal(t, "List of posts", responseBody["message"])
+
+	// Response data list posts
+	var listPosts = responseBody["data"].([]interface{})
+	// Response body data length not 0
+	assert.NotEqual(t, 0, len(listPosts))
+
+	// listPost the title after filter must be same title post that is search
+	assert.Equal(t, listPosts[0].(map[string]interface{})["title"], newPost.Title)
+}
+
+// TestGetListPostsUnauthorized as test unauthorized
+func TestGetListPostsUnauthorized(t *testing.T) {
+	// Open connection to db
+	db := util.SetupTestDb()
+
+	// Call router with argument db
+	router := router.SetupRouter(db)
+
+	// Create request with query filter by title
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/api/posts?", nil)
+	// Added header content type
+	request.Header.Add("Content-Type", "application/json")
+
+	// Create recorder
+	recorder := httptest.NewRecorder()
+
+	// Run server http
+	router.ServeHTTP(recorder, request)
+
+	// Get response
+	response := recorder.Result()
+
+	// Read response
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	// Decode json
+	json.Unmarshal(body, &responseBody)
+
+	// Response status code must be 401 (Unauthorized)
+	assert.Equal(t, 401, response.StatusCode)
+	// Response body status code must be 401 (Unauthorized)
+	assert.Equal(t, 401, int(responseBody["code"].(float64)))
+	// Response body status must be error
+	assert.Equal(t, "error", responseBody["status"])
+	// Response body message
+	assert.Equal(t, "Unauthorized", responseBody["message"])
+}
