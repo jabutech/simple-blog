cf662d59678549bc6649590ff4f7b2f0d0351445
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-06-10 10:30:37 +0700

    feature: change name CreateOrUpdatePostInput to CreatePostInput and added UpdatePostInput struct

--- post/input.go
+++ post/input.go
@@ -1,6 +1,11 @@
 package post
 
-type CreateOrUpdatePostInput struct {
+type CreatePostInput struct {
 	Title  string `json:"title" binding:"required"`
-	UserId string `json:"user_id" binding:"required"`
+	UserId string `uri:"user_id" binding:"required"`
+}
+
+type UpdatePostInput struct {
+	Title  string `json:"title" binding:"required"`
+	PostId string `uri:"post_id" binding:"required"`
 }
