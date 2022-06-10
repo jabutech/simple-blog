7d42b813c8d0cdff4f9300e163af6846b7683829
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-06-10 10:21:31 +0700

    refactor: change name struct, and set property userId is required

--- post/input.go
+++ post/input.go
@@ -1,6 +1,6 @@
 package post
 
-type CreatePostInput struct {
+type CreateOrUpdatePostInput struct {
 	Title  string `json:"title" binding:"required"`
-	UserId string `json:"user_id"`
+	UserId string `json:"user_id" binding:"required"`
 }
